c7799ba3456d2d4155a7fe0e55c72ba4aaad0b7d
Tero Tapio <teromarkustapio@gmail.com>
2018-09-21 15:54:44 +0300

    1.3

--- part_1/osa1/src/index.js
+++ part_1/osa1/src/index.js
@@ -3,20 +3,26 @@ import ReactDOM from 'react-dom';
 
 const App = () => {
     const kurssi = 'Half Stack -sovelluskehitys'
-    const osa1 = 'Reactin perusteet'
-    const tehtavia1 = 10
-    const osa2 = 'Tiedonvälitys propseilla'
-    const tehtavia2 = 7
-    const osa3 = 'Komponenttien tila'
-    const tehtavia3 = 14
+    const osa1 = {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10
+    }
+    const osa2 = {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7
+    }
+    const osa3 = {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14
+    }
   
     return (
       <div>
         <Otsikko title={kurssi} />
-        <Sisalto first={osa1} first_tasks={tehtavia1}
-            second={osa2} second_tasks={tehtavia2}
-            third={osa3} third_tasks={tehtavia3}/>
-        <Yhteensa ft={tehtavia1} st={tehtavia2} tt={tehtavia3} />
+        <Sisalto first={osa1.nimi} first_tasks={osa1.tehtavia}
+            second={osa2.nimi} second_tasks={osa2.tehtavia}
+            third={osa3.nimi} third_tasks={osa3.tehtavia}/>
+        <Yhteensa ft={osa1.tehtavia} st={osa2.tehtavia} tt={osa3.tehtavia} />
       </div>
     )
 }
